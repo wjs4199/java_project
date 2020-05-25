2100baf927a29019f0e925b28c1d6748793322b8
wjs4199 <63443458+wjs4199@users.noreply.github.com>
2020-05-25 19:03:28 +0900

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,48 @@
+public class Player {
+	
+	private String name;      //player name
+	private int[] score;      //player's score 
+	private boolean outcome;  //whether the player wins or loses.
+	
+	public Player() {
+		name= null;
+		score= new int[10];
+	}
+	
+	public Player(String name) {
+		this.name= name;
+		score= new int[10];
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(int playerscore, int i) {
+		score[i]=playerscore;
+	}
+	
+	public void setOutcome(boolean outcome) {
+		this.outcome=outcome;
+	}
+	
+	public boolean getOutcome() {
+		return outcome;
+	}
+	
+	//player's total sum of score
+	public int sum_score() {
+		int sum=0;
+		for(int i=0;i<10;i++) {
+			sum+= score[i];
+		}
+		return sum;
+	}
+	
+	//player's average of total sum of score
+	public double average_score() {
+		return Math.round(sum_score()/10*100)/100.0;
+	}
+
+	
+}
